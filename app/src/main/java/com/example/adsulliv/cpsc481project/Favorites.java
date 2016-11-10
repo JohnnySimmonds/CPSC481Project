5f73a41047cb4961de2d008827bd1573b8d6608a
scampb <scampb@cs.ucalgary.ca>
2016-11-10 16:39:13 -0700

    added horizontal scrolling view to the favorites page

--- app/src/main/java/com/example/adsulliv/cpsc481project/Favorites.java
+++ app/src/main/java/com/example/adsulliv/cpsc481project/Favorites.java
@@ -1,13 +1,29 @@
 package com.example.adsulliv.cpsc481project;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class Favorites extends AppCompatActivity {
+    ImageView imageView;
+    RoundImage roundImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
+
+        LinearLayout ll = (LinearLayout)findViewById(R.id.firstRow);
+        final int childCount = ll.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            imageView = (ImageView)ll.getChildAt(i);
+
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.jack);
+            roundImage = new RoundImage(bitmap);
+            imageView.setImageDrawable(roundImage);
+        }
     }
 }
