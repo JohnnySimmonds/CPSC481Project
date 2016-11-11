02f5ae450601844dc9a2af060abf49062532f50f
Austin Sullivan <austinmclastro@gmail.com>
2016-11-10 17:43:54 -0700

    More fleshed out activity finder functionality, now can switch between multiple images. Nonfunctional X and check button implemented.

--- app/src/main/java/com/example/adsulliv/cpsc481project/ActivityFinder.java
+++ app/src/main/java/com/example/adsulliv/cpsc481project/ActivityFinder.java
@@ -1,33 +1,47 @@
 package com.example.adsulliv.cpsc481project;
 
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+
 import static com.example.adsulliv.cpsc481project.R.id.activityImage;
-import static com.example.adsulliv.cpsc481project.R.id.image;
-import static com.example.adsulliv.cpsc481project.R.styleable.View;
 
 public class ActivityFinder extends AppCompatActivity {
+    //ArrayList<String> activityImages = new ArrayList<String>();       //dynamic list of image names
+    //activityImages.add(R.drawable.stockskimap1);
+
+    int drawableIndex = R.drawable.stockhike1;      //int to keep track of which drawable the activity finder is looking at
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finder);
 
-        Button buttonBrowseLeft = (Button)findViewById(R.id.buttonBrowseLeft);  //get instance of button to work on
+        Button buttonBrowseLeft = (Button)findViewById(R.id.buttonBrowseLeft);  //get instance of left arrow button
+        Button buttonBrowseRight = (Button)findViewById(R.id.buttonBrowseRight);  //get instance of right arrow button
+
 
         buttonBrowseLeft.setOnClickListener(new android.view.View.OnClickListener() {
-            ImageView activityImage = (ImageView)findViewById(R.id.activityImage);  //get instance of image to work on
-            //Drawable nextimage = getResources().getDrawable(R.drawable.stockskimap1);
+            ImageView activityImage = (ImageView)findViewById(R.id.activityImage);  //get instance of outdoor activity image
+
+        @Override
+        public void onClick(View view) {
+            drawableIndex -= 1;
+            activityImage.setImageResource(drawableIndex);
+            }
+        });
+
+        buttonBrowseRight.setOnClickListener(new android.view.View.OnClickListener() {
+            ImageView activityImage = (ImageView)findViewById(R.id.activityImage);  //get instance of outdoor activity image
 
             @Override
             public void onClick(View view) {
-                //activityImage.setImageResource(R.drawable.stockskimap1);
-                //activityImage.setBackground(nextimage);
+                drawableIndex += 1;
+                activityImage.setImageResource(drawableIndex);    //next outdoor activity image
             }
         });
     }
--- app/src/main/java/com/example/adsulliv/cpsc481project/MainMenu.java
+++ app/src/main/java/com/example/adsulliv/cpsc481project/MainMenu.java
@@ -1,14 +1,9 @@
 package com.example.adsulliv.cpsc481project;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-
-import static com.example.adsulliv.cpsc481project.R.id.activityImage;
 
 public class MainMenu extends AppCompatActivity {
 
