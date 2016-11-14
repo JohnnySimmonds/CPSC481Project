8b9b44887c1745bf1ed3d0a65548d5b811d9b663
JohnnySimmonds <simmondsjohnny@gmail.com>
2016-11-13 21:27:57 -0800

    Able to take photos with build in camera and from gallery and display on screen

--- app/src/main/java/com/example/adsulliv/cpsc481project/CreateActivity.java
+++ app/src/main/java/com/example/adsulliv/cpsc481project/CreateActivity.java
@@ -1,18 +1,22 @@
 package com.example.adsulliv.cpsc481project;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
+import android.graphics.BitmapRegionDecoder;
+import android.graphics.Rect;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.IOException;
+
+
 
 /*
 TO DO
@@ -23,7 +27,8 @@ store photo as drawable so that the activity finder can view it
 
 
 public class CreateActivity extends AppCompatActivity implements View.OnClickListener {
-
+    private static int MY_REQUEST_CAMERA = 1; //my request code for accessing the camera
+    private static int MY_REQUEST_GALLERY = 2; //my request code for accessing the gallery
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,19 +42,55 @@ public class CreateActivity extends AppCompatActivity implements View.OnClickLis
     /*open camera when button is pressed*/
 
     public void onClick(View view) {
+        Intent intent;
+        switch(view.getId()) {
 
-        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, 1);
+            case R.id.camera: {
+                 intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //create the intent for using the camera
 
-    }
-    /*stores the image data from the startActivityForResult intent in the imageview so that it can be displayed on screen*/
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Bundle extras = data.getExtras();
-        Bitmap imageBitmap = (Bitmap) extras.get("data");
-        if (requestCode == 1) {
-            ImageView pic = (ImageView) findViewById(R.id.camImg);
-            pic.setImageBitmap(imageBitmap);
+
+                if (intent.resolveActivity(getPackageManager()) != null) { //Protects app from crashing as if no intent can be handled the app would crash
+                    startActivityForResult(intent, MY_REQUEST_CAMERA); //start intent with a result so that we can store the image afterwards
+
+                }
+            }
+            case R.id.gallery:
+            {
+                intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+               // intent.setType("image/*"); dont think this is needed
+                if (intent.resolveActivity(getPackageManager()) != null) {
+
+                    startActivityForResult(intent, MY_REQUEST_GALLERY); //view gallery
+                }
+            }
         }
     }
+
+    /*stores the image data from the startActivityForResult intent in the imageview so that it can be displayed on screen*/
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView pic=null;
+        /* Store the photo taken from the camera as a bitmap to be displayed on screen*/
+        if (requestCode == MY_REQUEST_CAMERA) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            pic = (ImageView) findViewById(R.id.camImg);
+            pic.setImageBitmap(imageBitmap);
+        }
+        /* Store the photo taken from the gallery as a bitmap to be displayed on screen*/
+        else if(requestCode == MY_REQUEST_GALLERY)
+        {
+            Uri selectedImage = data.getData();
+            try {
+
+                Bitmap bm = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                pic = (ImageView) findViewById(R.id.camImg);
+                pic.setImageBitmap(bm);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
 
 }
