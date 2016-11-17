74276031eb0777dd7c248f9c5ccf85baeb038613
john.simmonds <john.simmonds@ucalgary.ca>
2016-11-17 12:22:18 -0700

    have it so we can read text from a file (as well as the image) when the post activity button is pressed from the edit texts still needs more work though

--- app/src/main/java/com/example/adsulliv/cpsc481project/CreateActivity.java
+++ app/src/main/java/com/example/adsulliv/cpsc481project/CreateActivity.java
@@ -20,11 +20,14 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 
 
@@ -37,8 +40,18 @@ store photo as drawable so that the activity finder can view it
 
 
 public class CreateActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /* Global variables*/
+    public static int index = 0; // way of organizing images to be accessed later
     private static int MY_REQUEST_CAMERA = 1; //my request code for accessing the camera
     private static int MY_REQUEST_GALLERY = 2; //my request code for accessing the gallery
+    Bitmap imageBitmap = null;
+    ImageView pic = null;
+    EditText info = null;
+    EditText activity =  null;
+    EditText price = null;
+    File directory = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +60,10 @@ public class CreateActivity extends AppCompatActivity implements View.OnClickLis
         Button camBut = (Button) findViewById(R.id.camera);
         camBut.setOnClickListener(CreateActivity.this);
 
+        pic =  (ImageView) findViewById(R.id.camImg);
+        info = (EditText) findViewById(R.id.description);
+        activity = (EditText) findViewById(R.id.activity);
+        price = (EditText) findViewById(R.id.price);
         /*
         EditText editText = (EditText) findViewById(R.id.description);
         editText.setOnEditorActionListener(new TextView.OnEditorActionListener()
@@ -65,7 +82,7 @@ public class CreateActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     /*open camera when button is pressed*/
-    public static int index = 0; // way of organizing images to be accessed later
+
     public void onClick(View view) {
         Intent intent;
         switch(view.getId()) {
@@ -90,9 +107,55 @@ public class CreateActivity extends AppCompatActivity implements View.OnClickLis
                 }
             }
             break;
+            /* when activity is posted save to file and clear all fields*/
+            case R.id.postActivity:
+            {
+
+                pic.setVisibility(View.VISIBLE);
+                loadImageFromStorage(saveToInternalStorage(imageBitmap));
+
+                readFile();
+
+                pic.setImageBitmap(null);
+                pic.setVisibility(View.INVISIBLE);
+                info.setText(null);
+                activity.setText(null);
+                price.setText(null);
+            }
+            break;
+            default:
         }
     }
 
+    private void readFile()
+    {
+        //----------------------------------------------------------------------------------
+               //Reads from a file and from all text fields.
+        TextView test = (TextView) findViewById(R.id.test);
+        File file = new File(directory.getAbsolutePath(), "/activity" + --index + ".jpg");
+
+        try {
+
+            String testString = "";
+            FileInputStream instream = new FileInputStream(file);
+            if (instream != null) {
+                Bitmap bitmap = BitmapFactory.decodeStream(instream);
+                while(instream.available() > 0)
+                {
+
+
+                    testString += Character.toString((char)instream.read()); //gets one char
+
+                }
+                test.setText(testString);
+            }
+            instream.close();
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        //----------------------------------------------------------------------------------
+    }
     /*stores the image data from the startActivityForResult intent in the imageview so that it can be displayed on screen*/
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         ImageView pic=null;
@@ -102,22 +165,21 @@ public class CreateActivity extends AppCompatActivity implements View.OnClickLis
 
         if (requestCode == MY_REQUEST_CAMERA) {
             Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageBitmap = (Bitmap) extras.get("data");
             pic = (ImageView) findViewById(R.id.camImg);
             pic.setImageBitmap(imageBitmap);
             pic.setVisibility(View.VISIBLE);
-            loadImageFromStorage(saveToInternalStorage(imageBitmap));
+          //  loadImageFromStorage(saveToInternalStorage(imageBitmap));
         }
         /* Store the photo taken from the gallery as a bitmap to be displayed on screen*/
         else if(requestCode == MY_REQUEST_GALLERY) {
             Uri selectedImage = data.getData();
             
             try {
-                Bitmap bm = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
                 pic = (ImageView) findViewById(R.id.camImg);
-                pic.setImageBitmap(bm);
+                pic.setImageBitmap(imageBitmap);
                 pic.setVisibility(View.VISIBLE);
-                loadImageFromStorage(saveToInternalStorage(bm));
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -134,7 +196,7 @@ public class CreateActivity extends AppCompatActivity implements View.OnClickLis
         ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
 
         // Create the directory to save the image, named imageDir.
-        File directory = contextWrapper.getDir("imageDir", Context.MODE_PRIVATE);
+        directory = contextWrapper.getDir("imageDir", Context.MODE_PRIVATE);
 
         // Give the image it's name.
         File mypath = new File(directory, "activity" + index + ".jpg");
@@ -146,7 +208,9 @@ public class CreateActivity extends AppCompatActivity implements View.OnClickLis
 
             // Compress the bitmap to write the image to the output stream.
             bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
-
+            outputStream.write(info.getText().toString().getBytes()); //stores the string in info into byte to be accessed later
+            outputStream.write(activity.getText().toString().getBytes());//stores the string in activity into byte to be accessed later
+            outputStream.write(price.getText().toString().getBytes());//stores the string in price into byte to be accessed later
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -165,11 +229,12 @@ public class CreateActivity extends AppCompatActivity implements View.OnClickLis
      */
     private void loadImageFromStorage(String path) {
         try {
-            File file = new File(path, "activity" + index-- + ".jpg");
+            File file = new File(path, "activity" + --index + ".jpg");
+            index++;
             Bitmap bitmap = BitmapFactory.decodeStream(new FileInputStream(file));
             ImageView imageView = (ImageView)findViewById(R.id.dummy);
             imageView.setImageBitmap(bitmap);
-
+            imageView.setVisibility(View.VISIBLE);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
