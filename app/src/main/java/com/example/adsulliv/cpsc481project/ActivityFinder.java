6f8f054ed8b2a51737086f4308fd3497ff80f9f4
Austin Sullivan <austinmclastro@gmail.com>
2016-11-18 13:50:37 -0700

    a

--- app/src/main/java/com/example/adsulliv/cpsc481project/ActivityFinder.java
+++ app/src/main/java/com/example/adsulliv/cpsc481project/ActivityFinder.java
@@ -4,11 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
-
-import java.util.ArrayList;
-
-import static com.example.adsulliv.cpsc481project.R.id.activityImage;
 
 public class ActivityFinder extends AppCompatActivity {
     //ArrayList<String> activityImages = new ArrayList<String>();       //dynamic list of image names
@@ -16,33 +13,67 @@ public class ActivityFinder extends AppCompatActivity {
 
     int drawableIndex = R.drawable.stockhike1;      //int to keep track of which drawable the activity finder is looking at
 
+    int index = 1;                      //current index of activity finder (which image is being viewed)
+    int amountActivities = 4;           //amount of activities in activity finder
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finder);
 
-        Button buttonBrowseLeft = (Button)findViewById(R.id.buttonBrowseLeft);  //get instance of left arrow button
-        Button buttonBrowseRight = (Button)findViewById(R.id.buttonBrowseRight);  //get instance of right arrow button
-
+        Button buttonBrowseLeft = (Button) findViewById(R.id.buttonBrowseLeft);  //get instance of left arrow button
+        Button buttonBrowseRight = (Button) findViewById(R.id.buttonBrowseRight);  //get instance of right arrow button
+        ImageButton buttonRedX = (ImageButton) findViewById(R.id.buttonRedX);       //get instance of red x button
+        ImageButton buttonGreenCheck = (ImageButton) findViewById(R.id.buttonGreenCheck);   //get instance of green check button
 
         buttonBrowseLeft.setOnClickListener(new android.view.View.OnClickListener() {
-            ImageView activityImage = (ImageView)findViewById(R.id.activityImage);  //get instance of outdoor activity image
-
-        @Override
-        public void onClick(View view) {
-            drawableIndex -= 1;
-            activityImage.setImageResource(drawableIndex);
-            }
-        });
-
-        buttonBrowseRight.setOnClickListener(new android.view.View.OnClickListener() {
-            ImageView activityImage = (ImageView)findViewById(R.id.activityImage);  //get instance of outdoor activity image
+            ImageView activityImage = (ImageView) findViewById(R.id.activityImage);  //get instance of outdoor activity image
 
             @Override
             public void onClick(View view) {
-                drawableIndex += 1;
-                activityImage.setImageResource(drawableIndex);    //next outdoor activity image
+                if (index > 0) {
+                    index -= 1;
+                    drawableIndex -= 1;
+                    activityImage.setImageResource(drawableIndex);
+                }
             }
         });
+
+
+        buttonBrowseRight.setOnClickListener(new android.view.View.OnClickListener() {
+            ImageView activityImage = (ImageView) findViewById(R.id.activityImage);  //get instance of outdoor activity image
+
+            @Override
+            public void onClick(View view) {
+                if (index < amountActivities - 1){
+                    index += 1;
+                    drawableIndex += 1;
+                    activityImage.setImageResource(drawableIndex);    //next outdoor activity image
+                }
+            }
+
+        });
+
+
+        buttonRedX.setOnClickListener(new android.view.View.OnClickListener() {
+            ImageView activityImage = (ImageView) findViewById(R.id.activityImage);  //get instance of outdoor activity image
+
+            @Override
+            public void onClick(View view) {
+               //
+            }
+
+        });
+
+        buttonGreenCheck.setOnClickListener(new android.view.View.OnClickListener() {
+            ImageView activityImage = (ImageView) findViewById(R.id.activityImage);  //get instance of outdoor activity image
+
+            @Override
+            public void onClick(View view) {
+                //
+            }
+
+        });
+
     }
 }
